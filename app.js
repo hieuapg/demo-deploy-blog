1ce000bc61b0bad7462c800957886d4aad363361
hieuapg <146419538+hieuapg@users.noreply.github.com>
2024-04-29 04:54:04 +0000

    added about

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
-const path = require('path')
+const path = require('path');
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+// Set the directory for views
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(__dirname))
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
+// Route handler for the root route
 app.get('/', (req, res) => {
   res.render('index.ejs');
+});
+
+// Route handler for the '/about' route
+app.get('/about', (req, res) => {
+  res.render('components/about.ejs');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('components/contact.ejs');
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
