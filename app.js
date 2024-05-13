4c1bf0bc3d4179732b8f6dde53041e395608f526
Hieu Tran <tranthanhhieu982@gmail.com>
2024-05-13 16:45:54 +0700

    deploy blog

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const articleRouter = require('./views/components/articles/routes/articles')
+
 const port = 3000;
 const path = require('path');
 
+app.use('/blog', articleRouter)
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 // Set the directory for views
@@ -23,6 +26,20 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('components/contact.ejs');
+});
+
+app.get('/blog', (req, res) => {
+  const blog = [{
+    title: 'Test Blog',
+    createdAt: new Date(),
+    description: 'Test description',
+  },
+  {
+    title: 'Test Blog',
+    createdAt: new Date(),
+    description: 'Test description',
+  }];
+  res.render('components/articles/blog.ejs', { blog: blog });
 });
 
 app.listen(port, () => {
